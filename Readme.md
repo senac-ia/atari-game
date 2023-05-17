7c8d805231b0a46148d7222d8e205a6b4b7bca7b
Celso Crivelaro <celsocrivelaro@gmail.com>
2023-05-16 21:54:08 -0300

    Dados

---
+++ Readme.md
@@ -0,0 +1,5 @@
+#pip install gymnasium[atari, accept-rom-licesnse]
+# https://github.com/mgbellemare/Arcade-Learning-Environment#rom-management
+
+#pip install gymnasium[atari]
+#pip install gymnasium[accept-rom-license]
--- main.py
+++ main.py
@@ -1,23 +1,36 @@
 import gymnasium
-import os
 
-#pip install gymnasium[atari, accept-rom-licesnse]
-# https://github.com/mgbellemare/Arcade-Learning-Environment#rom-management
+# https://pypi.org/project/colorama/
+from colorama import just_fix_windows_console, init, Back
 
-#pip3 install "autorom[accept-rom-license]"
-#env = gym.make("LunarLander-v2", render_mode="human")
+init(autoreset=True)
 
-#pip install gymnasium[atari]
-#pip install gymnasium[accept-rom-license]
+# use Colorama to make Termcolor work on Windows too
+just_fix_windows_console()
 
-env = gymnasium.make("ALE/Asteroids-v5", render_mode="human", obs_type="grayscale")
+env = gymnasium.make("ALE/Tennis-v5", render_mode="human", obs_type="ram")
 observation, info = env.reset(seed=42)
 #for _ in range(1000):
 while True:
-  action = env.action_space.sample()  # this is where you would insert your policy
-  observation, reward, terminated, truncated, info = env.step(action)
-  print("action: ", action)
+  # definição da política
+  acao = env.action_space.sample()
 
-  if terminated or truncated:
-    observation, info = env.reset()
+  ambiente, recompensa, finalizado, paralizado, info = env.step(acao)
+  recompensa_txt = "Recompensa: [%s]" % recompensa
+  fim_txt = "Finalizado: [%s]" % finalizado
+  acao_txt = "Ação: [%s]" % acao
+
+  if (recompensa < 0):
+    recompensa_txt = Back.RED + recompensa_txt + Back.RESET
+  elif (recompensa > 0):
+    recompensa_txt = Back.GREEN + recompensa_txt + Back.RESET
+
+  if (finalizado):
+    fim_txt = Back.RED + fim_txt + Back.RESET
+
+  print(recompensa_txt, fim_txt, acao_txt)
+
+  if finalizado or paralizado:
+    break
+    #ambiente, info = env.reset()
 env.close()
