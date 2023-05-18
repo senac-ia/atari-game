4236ff633df810ba459660798c744a1fea18697f
Celso Crivelaro <celsocrivelaro@gmail.com>
2023-05-17 21:41:34 -0300

    imprime status

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import gymnasium
 
 # https://pypi.org/project/colorama/
-from colorama import just_fix_windows_console, init, Back
+from colorama import just_fix_windows_console, init, Back, Fore
 
 init(autoreset=True)
 
@@ -9,26 +9,37 @@ init(autoreset=True)
 just_fix_windows_console()
 
 env = gymnasium.make("ALE/Tennis-v5", render_mode="human", obs_type="ram")
-observation, info = env.reset(seed=42)
-#for _ in range(1000):
+ambiente, info = env.reset(seed=42)
+
+def convert(n):
+  out = 1
+  # https://realpython.com/python-bitwise-operators/
+  for bit in n:
+    out = (out << 1) | bit
+  return out
+
+ambiente_txt = "Ambiente [%s]" % convert(ambiente)
+print(ambiente_txt, "Tamanho: " + str(len(ambiente)))
+
 while True:
   # definição da política
   acao = env.action_space.sample()
 
   ambiente, recompensa, finalizado, paralizado, info = env.step(acao)
+  ambiente_txt = "Ambiente [%s]" % convert(ambiente)
   recompensa_txt = "Recompensa: [%s]" % recompensa
   fim_txt = "Finalizado: [%s]" % finalizado
   acao_txt = "Ação: [%s]" % acao
 
   if (recompensa < 0):
-    recompensa_txt = Back.RED + recompensa_txt + Back.RESET
-  elif (recompensa > 0):
-    recompensa_txt = Back.GREEN + recompensa_txt + Back.RESET
+    recompensa_txt = Fore.RED + recompensa_txt + Fore.RESET
+  elif (recompensa >= 0):
+    recompensa_txt = Fore.GREEN + recompensa_txt + Fore.RESET
 
   if (finalizado):
-    fim_txt = Back.RED + fim_txt + Back.RESET
+    fim_txt = Fore.RED + fim_txt + Fore.RESET
 
-  print(recompensa_txt, fim_txt, acao_txt)
+  print(ambiente_txt, recompensa_txt, fim_txt, acao_txt)
 
   if finalizado or paralizado:
     break
